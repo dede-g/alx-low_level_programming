205f863153d7d9549fb898292fe1386db7b26dc8
dede-g <dedeteddy3@gmail.com>
2023-02-26 07:28:31 -0800

    Character is a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if the input is a digit (0 through 9)
+ * @c - an input character
+ * Return: 1 if c is a digit, 0 if otherwise
+ */
+
+int _isdigit(int c)
+{
+
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
