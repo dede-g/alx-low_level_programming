6dc7d32dfc112a9d549281f5ffcb248b6dfc1d39
dede-g <dedeteddy3@gmail.com>
2023-03-06 12:25:17 -0800

    Solving errors in the code

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,12 +7,10 @@
  */
 void print_rev(char *s)
 {
-	int i = 0, j;
+	int i, j;
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
+	for (i = 0; s[i] != '\0'; i++)
+		;
 	for (j = i; j >= 0; j--)
 	{
 		_putchar(s[j]);
