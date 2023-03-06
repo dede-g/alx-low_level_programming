fc85f94984799b15dd97e024a4cc007d04cd6cf8
dede-g <dedeteddy3@gmail.com>
2023-03-06 12:06:22 -0800

    Revised my code

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,16 +7,15 @@
  */
 void print_rev(char *s)
 {
-	int count = 0;
+	int i = 0, j;
 
-	while (count >= 0)
+	while (s[i] != '\0')
 	{
-		if (s[count] == '\0')
-			break;
-		count++;
+		i++;
 	}
-
-	for (count--; count >= 0; count--)
-		_putchar(s[count]);
+	for (j = i; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
 	_putchar('\n');
 }
