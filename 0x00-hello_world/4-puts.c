293c085954ca2661fb3fa7434a451a4a85b5835a
dede-g <dedeteddy3@gmail.com>
2023-12-14 14:29:17 -0800

    Add file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program demonstrates the use of puts to print a string
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
