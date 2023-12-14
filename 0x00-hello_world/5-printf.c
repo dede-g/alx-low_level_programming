d1eb494694848b4e62886d66d82bb7588fe11c09
dede-g <dedeteddy3@gmail.com>
2023-12-14 14:51:41 -0800

    Add file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This program demonstrates the use of the printf function
+ * to print a string
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
