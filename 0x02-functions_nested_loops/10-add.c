0335f008b1c3dce9d21c89f5e7914b43788fc11d
dede-g <dedeteddy3@gmail.com>
2023-12-20 14:44:26 -0800

    Implement C orogram that prints the sum of two given inputs)

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers and returns the result
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
