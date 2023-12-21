9ac8f3edb4fadd1e39408a4ea7b70206b1acac37
dede-g <dedeteddy3@gmail.com>
2023-12-21 15:08:03 -0800

    Implement C function thatbprints numner 1 to 100 following the FizzBuzz challenge

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * main - prints nbera from 1 to 100
+ * But print Fizz for multiplea of 3
+ * Buzz for multiples of 5
+ * FizzBuzz for multiples of both
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) == 0)
+			printf("FizzBuzz");
+		else if ((i % 3) == 0)
+			printf("Fizz");
+		else if ((i % 5) == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		
+		if (i != 100)
+			printf(" ");
+	}
+	printf("\n");
+
+	return (0);
+}
