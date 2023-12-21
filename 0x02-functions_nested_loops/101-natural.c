abc0afa8132d3516075284536ca57a0a3e1510cb
dede-g <dedeteddy3@gmail.com>
2023-12-21 09:57:32 -0800

    Implement progran that prints multiples of 5 and3 below 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - prints the sum of all the multiples of 3 and 5 below 1024
+ * followed by a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int sum, n;
+
+	for (n = 0; n < 1024; n++)
+	{
+		if (n % 3 == 0 | n % 5 == 0)
+		{
+			sum += n;
+		}
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
