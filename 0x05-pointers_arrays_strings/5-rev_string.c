8d7bd003d9b092f1413aaf663dff2c7022e30dca
dede-g <dedeteddy3@gmail.com>
2024-01-02 17:53:47 -0800

    Implement C function that reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string permanent
+ * @s: string to be reversed
+ *
+ * Return: Nothing
+ */
+
+void rev_string(char *s)
+{
+	int length = 0;
+	int start = 0;
+	int end;
+	char tmp;
+
+	while (s[length] != '\0')
+		length++;
+	end = length -1;
+
+	while (start < end)
+	{
+		tmp = s[start];
+		s[start] = s[end];
+		s[end] = tmp;
+
+		start++;
+		end--;
+	}
+}
