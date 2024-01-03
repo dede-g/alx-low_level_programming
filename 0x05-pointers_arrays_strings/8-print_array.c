d12ab62e51f1ae577d0b95dd037a156cc40973d1
dede-g <dedeteddy3@gmail.com>
2024-01-02 18:53:21 -0800

    Implement C function that prints a given number of elements of an array of integers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array of integers
+ * followed by a new line
+ * Numbers are separated by comma, followed by a space
+ *
+ * @a: given array
+ * @n: number of elements to print
+ *
+ * Return: Nothing
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != (n - 1))
+			printf(", ");
+	}
+	printf("\n");
+}
