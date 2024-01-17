09f55040fb0acf364495ea6051773ce4034741fb
dede-g <dedeteddy3@gmail.com>
2024-01-17 13:23:34 -0800

    Edit code style according to betty standards

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,7 +9,7 @@
  * @s1: first string
  * @s2: second string
  *
- * Return: pointer to a newly allocated space in memory which 
+ * Return: pointer to a newly allocated space in memory which
  * contains the contents of s1, followed by s2, and null terminated
  * NULL if failure
  */
@@ -18,7 +18,7 @@ char *str_concat(char *s1, char *s2)
 {
 	char *string;
 	unsigned int length;
-	
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
