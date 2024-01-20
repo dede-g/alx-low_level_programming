30be9f62d2c9333cf2100daf3bc0c8b376109c67
dede-g <dedeteddy3@gmail.com>
2024-01-19 22:16:57 -0800

    Add file

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -13,11 +13,21 @@
 int main(int argc, char *argv[])
 {
 	unsigned int result, num1, num2;
+	int i, j;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
+	}
+	for (i = 1; i <= 2; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				exit(98);
+			}
 	}
 
 	num1 = atoi(argv[1]);
