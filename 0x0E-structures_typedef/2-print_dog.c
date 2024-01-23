3812f3a124924b58911eb52b78095372204aabca
dede-g <dedeteddy3@gmail.com>
2024-01-23 11:27:02 -0800

    Add files

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * print_dog - print information of dog
+ * @d: pointer to struct dog
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		printf("Name: nil\n");
+	if (d->owner == NULL)
+		printf("Owner: nil\n");
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,11 @@
 #define DOG_H
 
 /**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
  * struct dog - A data structure that stores information of a dog
  * @name: name of dog
  * @age: age of dog
@@ -13,6 +18,10 @@ struct dog
 	float age;
 	char *owner;
 };
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
