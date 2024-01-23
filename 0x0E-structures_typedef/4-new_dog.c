6f135b748b6158d12eb5e58af3d7501a579ccf57
dede-g <dedeteddy3@gmail.com>
2024-01-23 14:43:48 -0800

    Add files

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * new_dog - pointer to struct
+ * @name: nae of dog
+ * @age: its age
+ * @owner: the owner
+ * Return: pointer to struct
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *newptr;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	newptr = malloc(sizeof(dog_t));
+	if (newptr == NULL)
+	{
+		return (NULL);
+	}
+	newptr->name = strdup(name);
+	newptr->age = age;
+	newptr->owner = owner;
+
+	return (newptr);
+}
