70a50ec8974033ce121f832f92979d73f9feb05e
dede-g <dedeteddy3@gmail.com>
2024-02-09 22:21:59 -0800

    Add files

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index.
+ * @n: address of number
+ * @index: given index
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	*n = *n & ~(1 << index);
+	return (1);
+}
